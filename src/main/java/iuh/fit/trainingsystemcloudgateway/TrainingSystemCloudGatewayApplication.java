2b783b1df6f0985f779de4d9ca7574a57faec699
Minh Thuan <nmthuan0712@gmail.com>
2023-11-15 20:09:41 +0700

    .

--- src/main/java/iuh/fit/trainingsystemcloudgateway/TrainingSystemCloudGatewayApplication.java
+++ src/main/java/iuh/fit/trainingsystemcloudgateway/TrainingSystemCloudGatewayApplication.java
@@ -2,12 +2,23 @@ package iuh.fit.trainingsystemcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class TrainingSystemCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingSystemCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSystemCloudGatewayApplication.class, args);
+    }
 
+    @Bean
+    public KeyResolver remoteAddrKeyResolver() {
+        return exchange -> Mono.just("1");
+    }
 }
